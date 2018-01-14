c7ca54bd746149247ab1e3e068cfcd3a7f758754
Kunal Gupta <kunal.gupta@myself.com>
2018-01-14 06:33:09 +0530

    Nice Main Feed.

--- server/routing.py
+++ server/routing.py
@@ -4,37 +4,37 @@ import ggtwitter , gginsta , ggfb
 rate_dic={}
 
 def total(fb,insta,twitter):
-	
-	post_total=[]
-	video_total=[]
-	photo_total=[]
-	
-	if twitter:
-		twitter=twitter.replace(' ','').split(',')
-		for link in twitter:
-			twitter_dict = ggtwitter.GGTwitter(page=link,seconds=60*60*24*5, no_of_posts=5) 	
-			twitter_dict = twitter_dict.final_data
-			post_total+=twitter_dict['post']
-			#video_total+= twitter_dict['video']
-		 	photo_total += twitter_dict['photo']
-	if fb : 
-		fb=fb.replace(' ','').split(',')
-		for link in fb:
-			fb_dict = ggfb.GGFB(url=link,seconds=60*60*24*5, no_of_posts=5)
-		 	fb_dict=fb_dict.fun_all()
-		 	post_total += fb_dict['post']
-		 	
-	if insta:	 	
-		insta=insta.split(',')
-		for link in insta:
-			insta_dict = gginsta.GGInsta(page=link,seconds=60*60*24*5, no_of_posts=5)
-			insta_dict = insta_dict.generate_list()
-			video_total+= insta_dict['video']
-		 	photo_total += insta_dict['photo']
+    
+    post_total=[]
+    video_total=[]
+    photo_total=[]
+    
+    if twitter:
+        twitter=twitter.replace(' ','').split(',')
+        for link in twitter:
+            twitter_dict = ggtwitter.GGTwitter(page=link,seconds=60*60*24*5, no_of_posts=5)     
+            twitter_dict = twitter_dict.final_data
+            post_total+=twitter_dict['post']
+            #video_total+= twitter_dict['video']
+            photo_total += twitter_dict['photo']
+    if fb : 
+        fb=fb.replace(' ','').split(',')
+        for link in fb:
+            fb_dict = ggfb.GGFB(url=link,seconds=60*60*24*5, no_of_posts=5)
+            fb_dict=fb_dict.fun_all()
+            post_total += fb_dict['post']
+             
+    if insta:         
+        insta=insta.split(',')
+        for link in insta:
+            insta_dict = gginsta.GGInsta(page=link,seconds=60*60*24*5, no_of_posts=5)
+            insta_dict = insta_dict.generate_list()
+            video_total+= insta_dict['video']
+            photo_total += insta_dict['photo']
 
 
 
-	return post_total+video_total+photo_total
+    return post_total+video_total+photo_total
 
 
 @route('/js/<filename>')
@@ -53,56 +53,56 @@ def img_static(filename):
 
 
 @route('/rate')
-	def rate():
-		final_dict=rate_dic.copy()
-		final_dict = sorted(final_dict, key=lambda k: k['rate'], reverse=True)
-		for post in final_dict:
-			if post['channel']=='fb':
-				#a,b=post['embed'].split('_')
-				embed.append(['fb',post['embed'],post['rate']])
-			elif post['channel']=='insta':
-				embed.append(['insta',post['url'],post['rate']])
-			elif post['channel']=='twitter':
-				embed.append(['twitter',post['url'],post['rate']])
+def rate():
+    final_dict=rate_dic.copy()
+    final_dict = sorted(final_dict, key=lambda k: k['rate'], reverse=True)
+    for post in final_dict:
+        if post['channel']=='fb':
+            #a,b=post['embed'].split('_')
+            embed.append(['fb',post['embed'],post['rate']])
+        elif post['channel']=='insta':
+            embed.append(['insta',post['url'],post['rate']])
+        elif post['channel']=='twitter':
+            embed.append(['twitter',post['url'],post['rate']])
 
-			
+            
 
-	
-	
-	return template('rate',emb=embed)
+    
+    
+    return template('rate',emb=embed)
 
 
 @route('/')
 def index():
-	return template('index')
+    return template('index')
 
 @route('/site',method='POST')
 def site():
-	fb=request.POST.get('fb')
-	insta=request.POST.get('insta')
-	twitter=request.POST.get('twitter')
-	final_dict=total(fb,insta,twitter)
- 	global rate_dic 
- 	rate_dic = final_dict	
-	embed=[]
-	
+    fb=request.POST.get('fb')
+    insta=request.POST.get('insta')
+    twitter=request.POST.get('twitter')
+    final_dict=total(fb,insta,twitter)
+    global rate_dic 
+    rate_dic = final_dict    
+    embed=[]
+    
 
-	final_dict = sorted(final_dict, key=lambda k: k['timestamp'], reverse=True)
-	for post in final_dict:
-			if post['channel']=='fb':
-				#a,b=post['embed'].split('_')
-				embed.append(['fb',post['embed']])
-			elif post['channel']=='insta':
-				embed.append(['insta',post['url']])
-			elif post['channel']=='twitter':
-				embed.append(['twitter',post['url']])
+    final_dict = sorted(final_dict, key=lambda k: k['timestamp'], reverse=True)
+    for post in final_dict:
+        if post['channel']=='fb':
+                #a,b=post['embed'].split('_')
+            embed.append(['fb',post['embed']])
+        elif post['channel']=='insta':
+                embed.append(['insta',post['url']])
+        elif post['channel']=='twitter':
+                embed.append(['twitter',post['url']])
 
-			
+            
 
-	
-	
-	return template('stream',emb=embed)
+    
+    
+    return template('stream',emb=embed)
 
 
 if __name__=='__main__':
-	run(debug=True,port=8866,reloader=True)	
+    run(debug=True,port=8866,reloader=True)    
--- server/views/stream.tpl
+++ server/views/stream.tpl
@@ -57,11 +57,11 @@
       </div>
     </header>
   <div  class='container text-center'>
-<div class='container text-center' width="60%"  >
+<div class='container text-center' width="530px" style="max-width: 530px;"  >
     
     	% for frame in emb:
     		% if frame[0]=='fb':
-    			<iframe   src="{{frame[1]}}&width=648" width="648" height="648" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+    			<iframe   src="{{frame[1]}}&width=500" width="550px" height="" scrolling="no" frameborder="0" allowTransparency="true" style="min-height:100%;"></iframe>
     		
   	
   
@@ -74,7 +74,7 @@
 <!--    			<blockquote class="instagram-media  text-center instagram"  data-instgrm-captioned data-instgrm-permalink={{ frame[1]}} style="max-width:658px; width:100%; margin: 0 auto !important;"> </blockquote> -->
 <!--<blockquote class="instagram-media text-center instagram" data-instgrm-captioned data-instgrm-permalink="{{ frame[1] }}" data-instgrm-version="8" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><div style="padding:8px;"> <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:37.5% 0; text-align:center; width:100%;"> <div style=" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div></div><p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;"><a href="https://www.instagram.com/p/BdqJEePHbGi/" style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none;" target="_blank">A post shared by Suryaveer Singh (@_suryaveer)</a> on <time style=" font-family:Arial,sans-serif; font-size:14px; line-height:17px;" datetime="2018-01-07T18:34:02+00:00">Jan 7, 2018 at 10:34am PST</time></p></div></blockquote> <script async defer src="//platform.instagram.com/en_US/embeds.js"></script>-->
 
-    <iframe class="instagram-media instagram-media-rendered" id="instagram-embed-0" src="{{frame[1]}}embed/captioned/?cr=1&amp;v=8&amp;wp=658#%7B%22ci%22%3A0%2C%22os%22%3A6292.755%7D" allowtransparency="true" frameborder="0" height="814" data-instgrm-payload-id="instagram-media-payload-0" scrolling="no" style="background: rgb(255, 255, 255); border: 1px solid rgb(219, 219, 219); margin: 0 auto; max-width: 658px; width: calc(100% - 2px); border-radius: 4px; box-shadow: none; display: block; padding: 0px;"></iframe>
+    <iframe class="instagram-media instagram-media-rendered" id="instagram-embed-0" src="{{frame[1]}}embed/captioned/?cr=1&amp;v=8&amp;wp=440#%7B%22ci%22%3A0%2C%22os%22%3A6292.755%7D" allowtransparency="true" frameborder="0" height="814" data-instgrm-payload-id="instagram-media-payload-0" scrolling="no" style="background: rgb(255, 255, 255); border: 1px solid rgb(219, 219, 219); margin: 0 auto; max-width: 550px; width: calc(100% - 2px); border-radius: 4px; box-shadow: none; display: block; padding: 0px;"></iframe>
 
 <!--<iframe class="instagram-media instagram-media-rendered" id="instagram-embed-4" src='{{frame[1]}}embed/?cr=1&v=8wp=658%23%7b%22ci%22%3a0%2c%22os%22%3a6988.285%7d' allowtransparency="true" frameborder="0" height="556" data-instgrm-payload-id="instagram-media-payload-4" scrolling="no" style="background: rgb(255, 255, 255); border: 1px solid rgb(219, 219, 219); margin: 0 auto; max-width: 658px; width: calc(100% - 2px); border-radius: 4px; box-shadow: none; display: block; padding: 0px;"></iframe>-->
     		
